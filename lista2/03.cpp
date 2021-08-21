659ae3316c501940b568e5eb0681e00e7e60677b
Clara Mendes <claraplanejamento@gmail.com>
2021-08-21 13:00:25 -0300

    :sparkles: adds 3 to lista2

--- lista2/03.cpp
+++ lista2/03.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 int main() {
-  int n;
+  int n, ans=0, p1, p2, p3, num;
   cin >> n;
-  
+
+  for (int i = 0; i < n; i++) {
+    cin >> p1 >> p2 >> p3;
+    num = 0;
+    if (p1 == 1) {
+      num++;
+    }
+    if (p2 == 1) {
+      num++;
+    }
+    if (p3 == 1) {
+      num++;
+    }
+    if (num >= 2) {
+      ans++;
+    }
+  }
+  cout << ans;
 }
